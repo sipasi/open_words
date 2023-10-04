cc7e4d548cebd75307fa078b3a546bb24e08afac
sipasi <tanksasha77@gmail.com>
2023-10-04 13:51:09 +0300

    feat: add custom tile

---
+++ lib/view/shared/tile/custom_tile.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:open_words/theme/theme_switcher.dart';
+
+class CustomTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  final void Function() onTap;
+
+  final EdgeInsetsGeometry margin;
+
+  const CustomTile({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.onTap,
+    this.margin = const EdgeInsets.all(14.0),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final mode = ThemeSwitcher.of(context).theme!.mode;
+
+    final themeTitle = theme.textTheme.bodyLarge?.copyWith(
+      color: theme.colorScheme.secondary,
+      fontWeight: FontWeight.bold,
+    );
+
+    final themeSubTitle = theme.textTheme.bodyMedium?.copyWith(
+      color: mode == ThemeMode.dark ? Colors.grey[400] : Colors.grey[600],
+    );
+
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: margin,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              title,
+              overflow: TextOverflow.ellipsis,
+              style: themeTitle,
+            ),
+            const Spacer(),
+            Text(
+              subtitle,
+              overflow: TextOverflow.ellipsis,
+              style: themeSubTitle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
