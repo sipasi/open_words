5c2f0da5cf20eb59bc8ceccb011416a67a9351b2
sipasi <tanksasha77@gmail.com>
2023-10-04 13:49:33 +0300

    feat: add in memory storage

---
+++ lib/storage/work_group_storage.dart
@@ -0,0 +1,4 @@
+import 'package:open_words/data/word/word_group.dart';
+import 'package:open_words/storage/entity_storage_async.dart';
+
+abstract class WorkGroupStorage extends EntityStorageAsync<String, WordGroup> {}
