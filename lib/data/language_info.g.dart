d49cb7d7771837141c08a1d6081c33469f54af27
sipasi <tanksasha77@gmail.com>
2023-10-04 13:44:38 +0300

    feat: word language model

---
+++ lib/data/language_info.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'language_info.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LanguageInfo _$LanguageInfoFromJson(Map<String, dynamic> json) => LanguageInfo(
+      code: json['code'] as String,
+      name: json['name'] as String,
+      native: json['native'] as String,
+    );
+
+Map<String, dynamic> _$LanguageInfoToJson(LanguageInfo instance) =>
+    <String, dynamic>{
+      'code': instance.code,
+      'name': instance.name,
+      'native': instance.native,
+    };
