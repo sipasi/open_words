67028343682fde0d0e74de4f225b5bb2956b7f9c
sipasi <tanksasha77@gmail.com>
2023-10-28 21:00:26 +0300

    chore: replace hard write metadata to generator

--- lib/view/word/detail/word_detail_page.dart
+++ lib/view/word/detail/word_detail_page.dart
@@ -4,6 +4,7 @@ import 'package:open_words/data/language_info.dart';
 import 'package:open_words/data/metadata/definition.dart';
 import 'package:open_words/data/metadata/meaning.dart';
 import 'package:open_words/data/metadata/word_metadata.dart';
+import 'package:open_words/data/metadata/word_metadata_genarator.dart';
 import 'package:open_words/data/word/word.dart';
 import 'package:open_words/service/clipboard_service.dart';
 import 'package:open_words/service/text_to_speech_service.dart';
@@ -13,37 +14,7 @@ import 'package:open_words/view/word/detail/metadata/word_metadata_view.dart';
 class WordDetailPage extends StatelessWidget {
   final Word word;
 
-  final WordMetadata metadata = WordMetadata(
-    id: 'id',
-    word: 'word.origin',
-    phonetics: List.empty(),
-    meanings: [
-      Meaning(
-        partOfSpeech: 'partOfSpeech',
-        definitions: [
-          Definition(
-            value: 'Cupidatat nostrud ut sint culpa aliquip sint excepteur consectetur.',
-            example: 'Do magna sit cupidatat irure laborum deserunt cupidatat non laborum ullamco.',
-          ),
-          Definition(
-            value: 'Amet enim exercitation non tempor nulla labore fugiat pariatur commodo.',
-            example: 'Proident excepteur veniam velit tempor anim duis commodo cupidatat.',
-          ),
-          Definition(
-            value: 'Culpa quis do cillum excepteur eiusmod commodo esse.',
-            example: 'Laborum ex nostrud proident tempor proident ut sit et nisi magna laborum aliqua nisi.',
-          ),
-          Definition(
-            value: 'Voluptate elit aliqua laborum ex occaecat in labore reprehenderit non.',
-            example: 'Nisi ut cupidatat pariatur laborum duis nisi.',
-          ),
-          Definition(value: 'Amet consectetur duis quis minim commodo anim aliqua proident aliqua qui fugiat in sit.')
-        ],
-        synonyms: List.generate(10, (index) => 'synonym $index'),
-        antonyms: List.generate(10, (index) => 'antonym $index'),
-      ),
-    ],
-  );
+  final WordMetadata metadata = WordMetadataGenarator.get();
 
   final LanguageInfo originLanguage;
   final LanguageInfo translationLanguage;
