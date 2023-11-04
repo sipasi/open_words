9869f8ad1169cf1c9d089392297297f4436e821d
“sasha” <fake@gmail.com>
2023-11-05 00:36:51 +0200

    chore: update AppDependencySetter add DevDictionaryApi, MetadataStorage and MetadataWebFinder

--- lib/dependency/dependency_setter.dart
+++ lib/dependency/dependency_setter.dart
@@ -1,6 +1,10 @@
 import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
 import 'package:open_words/service/clipboard_service.dart';
 import 'package:open_words/service/language/language_info_service.dart';
+import 'package:open_words/service/metadata/dev_dictionary_api.dart';
+import 'package:open_words/service/metadata/metadata_service.dart';
+import 'package:open_words/service/metadata/metadata_web_finder.dart';
 import 'package:open_words/service/text_to_speech_service.dart';
 import 'package:open_words/storage/metadata_storage.dart';
 import 'package:open_words/storage/storage_factory.dart';
@@ -18,5 +22,26 @@ abstract class AppDependencySetter {
     instance.registerSingleton<LanguageInfoService>(LanguageInfoService());
     instance.registerSingleton<TextToSpeechService>(TextToSpeechService());
     instance.registerSingleton<ClipboardService>(ClipboardService());
+
+    instance.registerSingleton(Logger());
+
+    registerMetadataServices(instance);
+  }
+
+  static void registerMetadataServices(GetIt instance) {
+    final webApi = DevDictionaryApi();
+
+    final webFinder = MetadataWebFinder(webApi);
+
+    instance.registerSingleton(webFinder);
+
+    instance.registerLazySingleton(() {
+      final ser = MetadataService(
+        storage: GetIt.I.get<MetadataStorage>(),
+        webFinder: GetIt.I.get<MetadataWebFinder>(),
+      );
+
+      return ser;
+    });
   }
 }
