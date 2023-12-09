3bf6378a3ce1998ca1d87e5ac183ebeb6bb9ea93
sipasi <tanksasha77@gmail.com>
2023-12-09 19:27:47 +0200

    chore: refactor settings

---
+++ lib/view/settings/export/import_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImportPage extends StatefulWidget {
+  const ImportPage({super.key});
+
+  @override
+  State<ImportPage> createState() => _ImportPageState();
+}
+
+class _ImportPageState extends State<ImportPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/view/settings/settings_page.dart
+++ lib/view/settings/settings_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:open_words/view/settings/theme_color_tile.dart';
-import 'package:open_words/view/settings/theme_mode_tile.dart';
+import 'package:open_words/service/navigation/material_navigator.dart';
+import 'package:open_words/view/settings/export/export_page.dart';
+import 'package:open_words/view/settings/export/import_page.dart';
+import 'package:open_words/view/settings/theme/theme_color_tile.dart';
+import 'package:open_words/view/settings/theme/theme_mode_tile.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -29,11 +32,17 @@ class SettingsPageState extends State<SettingsPage> {
         const Divider(),
         ListTile(
           leading: const Icon(Icons.file_download_outlined),
-          title: OutlinedButton(child: const Text('Import'), onPressed: () {}),
+          title: OutlinedButton(
+            child: const Text('Import'),
+            onPressed: () => MaterialNavigator.push(context, (context) => const ImportPage()),
+          ),
         ),
         ListTile(
           leading: const Icon(Icons.file_upload_outlined),
-          title: OutlinedButton(child: const Text('Export'), onPressed: () {}),
+          title: OutlinedButton(
+            child: const Text('Export'),
+            onPressed: () => MaterialNavigator.push(context, (context) => const ExportPage()),
+          ),
         ),
         const Divider(),
         ListTile(
