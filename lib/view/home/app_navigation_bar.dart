df91ae1a03b57cf70016f8b55f07852235147d4a
sipasi <tanksasha77@gmail.com>
2023-12-12 12:10:40 +0200

    feat: add abstractions that create rail or bottom navigation

---
+++ lib/view/home/app_navigation_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:open_words/view/home/navigation_destination_base.dart';
+
+class AppNavigationBar {
+  final int current;
+  final List<NavigationDestinationBase> items;
+
+  final void Function(int) selected;
+
+  late final List<NavigationRailDestination> _railDestination;
+  late final List<Widget> _bottomDestination;
+
+  AppNavigationBar({required this.current, required this.items, required this.selected}) {
+    _railDestination = items
+        .map((e) => NavigationRailDestination(
+              icon: Icon(e.icon),
+              label: Text(e.label),
+            ))
+        .toList();
+
+    _bottomDestination = items
+        .map((e) => NavigationDestination(
+              icon: Icon(e.icon),
+              label: e.label,
+            ))
+        .toList();
+  }
+
+  Widget asRail() {
+    return NavigationRail(
+      destinations: _railDestination,
+      selectedIndex: current,
+      onDestinationSelected: selected,
+    );
+  }
+
+  Widget asBottom() {
+    return NavigationBar(
+      selectedIndex: current,
+      onDestinationSelected: selected,
+      destinations: _bottomDestination,
+    );
+  }
+}
---
+++ lib/view/home/navigation_destination_base.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NavigationDestinationBase {
+  final IconData icon;
+  final String label;
+
+  const NavigationDestinationBase({required this.icon, required this.label});
+}
