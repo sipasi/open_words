e587259c1f16034bcd19815dc903f434039a3d86
sipasi <tanksasha77@gmail.com>
2024-05-05 00:36:27 +0300

    chore: Deprecated imperative apply of Flutter's Gradle plugins

--- android/app/src/main/kotlin/com/example/open_words/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/open_words/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.open_words
+package com.example.open_words_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
