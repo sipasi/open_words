2db70856e2cdeb8c4ceb6bfc8d8f40b4a374513b
sipasi <tanksasha77@gmail.com>
2024-05-30 21:45:44 +0300

    refactor: Refactor SembastHelper class for platform-specific database opening
    This commit refactors the SembastHelper class to provide platform-specific methods for opening databases. The changes are as follows:
    
    - Renamed the 'openDatabase' method to 'openOnDevice' to clarify its purpose.
    - Added a new method 'openOnWeb' to open databases specifically for web platforms.
    - Updated 'openForCurrentPlatform' to dynamically choose between opening on web or device based on the platform.
    - Modified database path generation to use the provided 'name' parameter, making it more flexible and reusable.
    
    These changes enhance the clarity and maintainability of the codebase, ensuring better compatibility across different platforms.

--- lib/storage/sembast/sembast_helper.dart
+++ lib/storage/sembast/sembast_helper.dart
@@ -1,17 +1,24 @@
+import 'package:flutter/foundation.dart';
 import 'package:path/path.dart' as path_provider;
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
+import 'package:sembast_web/sembast_web.dart';
 
 class SembastHelper {
-  static Future<Database> openDatabase() async {
+  static Future<Database> openOnDevice(String name) async {
     // get the application documents directory
     var directory = await path_provider.getApplicationDocumentsDirectory();
     // make sure it exists
     await directory.create(recursive: true);
     // build the database path
-    var path = path_provider.join(directory.path, 'word_group_sembast.db');
+    var path = path_provider.join(directory.path, '$name.db');
     // open the database
     return await databaseFactoryIo.openDatabase(path);
   }
+
+  static Future<Database> openOnWeb(String name) => databaseFactoryWeb.openDatabase('open_words_web');
+
+  static Future<Database> openForCurrentPlatform(String name) =>
+      kIsWeb ? SembastHelper.openOnWeb(name) : SembastHelper.openOnDevice(name);
 }
