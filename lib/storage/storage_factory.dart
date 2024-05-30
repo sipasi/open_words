ca55e6df40a8567fbe4e1208f3fb9cb9311e3f60
sipasi <tanksasha77@gmail.com>
2024-05-30 21:58:52 +0300

    refactor: Refactor StorageFactory to use sembast as default storage
    Refactored the StorageFactory class to use sembast as the default storage option. Introduced a new method `sembast` for initializing sembast storage with an optional name parameter. Removed the `createSembast` method and integrated its functionality into the `sembast` method. Updated the `createDefault` method to utilize the `sembast` method. Also adjusted the method names to adhere to Dart naming conventions.
    
    Changes:
    - Renamed `createSembast` to `sembast`.
    - Updated `createDefault` to use `sembast` method.
    - Introduced optional name parameter for `sembast` method.
    - Integrated functionality of `createSembast` into `sembast` method.
    - Adjusted method names to follow Dart naming conventions.

--- lib/storage/storage_factory.dart
+++ lib/storage/storage_factory.dart
@@ -1,33 +1,20 @@
-import 'dart:async' show Future;
-import 'package:flutter/foundation.dart';
-import 'package:open_words/storage/in_memory_storage_helper.dart';
 import 'package:open_words/storage/metadata_storage.dart';
 import 'package:open_words/storage/sembast/sembast_helper.dart';
 import 'package:open_words/storage/sembast/sembast_metadata_storege.dart';
 import 'package:open_words/storage/sembast/sembast_word_group_storege.dart';
 import 'package:open_words/storage/word_group_storage.dart';
-import 'package:sembast_web/sembast_web.dart';
 
 typedef StoragePair = (WordGroupStorage, MetadataStorage);
 
 class StorageFactory {
-  static Future<StoragePair> createSembast() async {
-    final databaseFuture = kIsWeb ? databaseFactoryWeb.openDatabase('open_words_web') : SembastHelper.openDatabase();
+  static Future<StoragePair> createDefault() => sembast();
 
-    final database = await databaseFuture;
+  static Future<StoragePair> sembast([String name = 'open-words-sembast']) async {
+    final database = await SembastHelper.openForCurrentPlatform(name);
 
     final groupStorege = SembastWordGroupStorege(database: database, name: 'word_group');
     final metadataStorege = SembastMetadataStorege(database: database, name: 'metadata');
 
     return (groupStorege, metadataStorege);
   }
-
-  static Future<StoragePair> createInMemory() async {
-    final groupStorege = await InMemoryStorageHelper.createWordGroup();
-    final metadataStorege = InMemoryStorageHelper.createMetadata();
-
-    return (groupStorege, metadataStorege);
-  }
-
-  static Future<StoragePair> createDefault() => createSembast();
 }
