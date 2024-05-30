1ed02e1bff2fe3e89b8706bfb049d67bd0b681d6
sipasi <tanksasha77@gmail.com>
2024-05-30 21:52:36 +0300

    refactor: Change key type to int in MetadataStorage and add 'firstByWord' method
    This commit modifies the MetadataStorage class to change the key type from String to int, enhancing efficiency and consistency with the underlying data structure. Additionally, it introduces a new method 'firstByWord' to retrieve the first WordMetadata associated with a given word.

--- lib/storage/metadata_storage.dart
+++ lib/storage/metadata_storage.dart
@@ -1,4 +1,6 @@
 import 'package:open_words/data/metadata/word_metadata.dart';
 import 'package:open_words/storage/entity_storage_async.dart';
 
-abstract class MetadataStorage extends EntityStorageAsync<String, WordMetadata> {}
+abstract class MetadataStorage extends EntityStorageAsync<int, WordMetadata> {
+  Future<WordMetadata?> firstByWord(String word);
+}
