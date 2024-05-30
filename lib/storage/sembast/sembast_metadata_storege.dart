e14066a3599c9ad715dc280e55fe9d85c86843b3
sipasi <tanksasha77@gmail.com>
2024-05-30 22:06:17 +0300

    feat: enhance SembastMetadataStorege with additional functionality
    Expanded the `SembastMetadataStorege` class by implementing the `firstByWord` method to retrieve metadata by word. Utilized the `store.findFirst` method with a custom Finder to perform the lookup efficiently. Added the abstract method `getId` to provide flexibility for setting IDs. These changes enhance the functionality and flexibility of the `SembastMetadataStorege` class.
    
    Changes:
    - Implemented the `firstByWord` method to retrieve metadata by word efficiently.
    - Utilized the `store.findFirst` method with a custom Finder to perform the lookup.
    - Added the abstract method `getId` to provide flexibility for setting IDs.

--- lib/storage/sembast/sembast_metadata_storege.dart
+++ lib/storage/sembast/sembast_metadata_storege.dart
@@ -1,6 +1,7 @@
 import 'package:open_words/data/metadata/word_metadata.dart';
 import 'package:open_words/storage/metadata_storage.dart';
 import 'package:open_words/storage/sembast/sembast_base_storage.dart';
+import 'package:sembast/sembast.dart';
 
 class SembastMetadataStorege extends SembastBaseStorage<WordMetadata> implements MetadataStorage {
   SembastMetadataStorege({required super.database, required super.name});
@@ -10,4 +11,21 @@ class SembastMetadataStorege extends SembastBaseStorage<WordMetadata> implements
 
   @override
   Map<String, dynamic> toJson(WordMetadata entity) => entity.toJson();
+
+  @override
+  Future<WordMetadata?> firstByWord(String word) async {
+    final result = await store.findFirst(
+      database,
+      finder: Finder(filter: Filter.equals('word', word)),
+    );
+
+    if (result == null) {
+      return null;
+    }
+
+    return fromJson(result.value);
+  }
+
+  @override
+  int? getId(WordMetadata entity) => entity.id;
 }
