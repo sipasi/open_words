e7b3ad94b1c68c9ecf08c0f521714cb58f340efe
sipasi <tanksasha77@gmail.com>
2024-05-30 21:51:14 +0300

    refactor: Change key type in WordGroupStorage to int
    This commit updates the key type in the WordGroupStorage class from String to int to enhance efficiency and consistency with the underlying data structure.

--- lib/storage/word_group_storage.dart
+++ lib/storage/word_group_storage.dart
@@ -1,4 +1,4 @@
 import 'package:open_words/data/word/word_group.dart';
 import 'package:open_words/storage/entity_storage_async.dart';
 
-abstract class WordGroupStorage extends EntityStorageAsync<String, WordGroup> {}
+abstract class WordGroupStorage extends EntityStorageAsync<int, WordGroup> {}
