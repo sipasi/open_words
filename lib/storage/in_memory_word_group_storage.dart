a7f28c2ee28c33bb540236594c8dc3b0b734de06
sipasi <tanksasha77@gmail.com>
2024-05-30 21:39:55 +0300

    feat: remove in memory storage

--- lib/storage/in_memory_word_group_storage.dart
+++
@@ -1,5 +0,0 @@
-import 'package:open_words/data/word/word_group.dart';
-import 'package:open_words/storage/in_memory_storage.dart';
-import 'package:open_words/storage/word_group_storage.dart';
-
-class InMemoryWordGroupStorage extends InMemoryStorage<WordGroup> implements WordGroupStorage {}
