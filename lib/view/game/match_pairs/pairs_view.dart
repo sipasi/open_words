e8c816a0a0a727373b1e7a1a0c6d862539df485a
sipasi <tanksasha77@gmail.com>
2024-07-03 00:16:03 +0300

    feat: implement PairsView widget for displaying pair-matching game UI
    - Introduced `PairsView` widget to display the UI for pair-matching games.
    - Includes title, progress indicator, pair elements (origin and translation), and navigation controls.
    - Utilizes `ExpandedColumn` for flexible layout of origin and translation elements.
    - Provides callbacks for exiting the game and progressing to the next game pack based on game state.
    - Uses `LinearProgressIndicator` to show the progress of answered pairs.
    - Designed with cohesive styling and layout using Flutter's `Scaffold`, `SafeArea`, and other layout widgets.
    
    This widget facilitates the visual representation and interaction of pair-matching games within Flutter applications.

---
+++ lib/view/game/match_pairs/pairs_view.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:open_words/view/game/match_pairs/expanded_column.dart';
+
+class PairsView extends StatelessWidget {
+  final String title;
+
+  final int pairsCount;
+
+  final double answeredPairsPercentage;
+
+  final bool lastPack;
+  final bool currentCompleted;
+
+  final Widget Function(int index) originBuilder;
+  final Widget Function(int index) translationBuilder;
+
+  final void Function() onGameExitTap;
+  final void Function() onNextPackTap;
+
+  const PairsView({
+    super.key,
+    required this.title,
+    required this.pairsCount,
+    required this.answeredPairsPercentage,
+    required this.lastPack,
+    required this.currentCompleted,
+    required this.originBuilder,
+    required this.translationBuilder,
+    required this.onGameExitTap,
+    required this.onNextPackTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(
+                title,
+                style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                child: LinearProgressIndicator(value: answeredPairsPercentage),
+              ),
+              _body(),
+              _bottom(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _body() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 30),
+        child: Row(
+          children: [
+            ExpandedColumn(
+              length: pairsCount,
+              builder: (index) => originBuilder(index),
+            ),
+            ExpandedColumn(
+              length: pairsCount,
+              builder: (index) => translationBuilder(index),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _bottom(BuildContext context) {
+    String nextButtonText = lastPack ? '' : 'Next';
+
+    return Row(
+      children: [
+        IconButton.filledTonal(
+          onPressed: onGameExitTap,
+          icon: const Icon(Icons.arrow_back),
+        ),
+        const SizedBox(width: 20),
+        Expanded(
+          child: FilledButton.tonal(
+            onPressed: currentCompleted ? onNextPackTap : null,
+            child: Text(nextButtonText),
+          ),
+        ),
+      ],
+    );
+  }
+}
