52703d9294badea56752da30bbdbdd23779ffb48
sipasi <tanksasha77@gmail.com>
2024-07-03 00:19:55 +0300

    feat: refactor UI and add new games: Match Word Pairs and Match Audio Pairs
    - Refactored `GameListPage` to improve UI structure and readability.
    - Introduced two new games:
      - **Match Word Pairs**: Allows players to match pairs of words.
      - **Match Audio Pairs**: Introduces a game where players match audio pairs.
    - Each game tile in `GameListPage` now checks word availability before allowing navigation, ensuring a minimum requirement of 5 words for both new games.
    - Enhancements include clearer UI sections ('Compare' and 'Match') and consistent use of Flutter's `Scaffold`, `AppBar`, `SafeArea`, `ListView`, and custom widgets (`AdaptiveGridView`, `_GameInfoTile`, `TextTile`, `Card.outlined`).
    
    This commit enhances gameplay options and improves the overall structure of game navigation within the application.

--- lib/view/game/game_list_page.dart
+++ lib/view/game/game_list_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:open_words/data/word/word_group.dart';
 import 'package:open_words/service/navigation/material_navigator.dart';
+import 'package:open_words/view/game/match_pairs/audio_pairs/audio_pairs_page.dart';
 import 'package:open_words/view/game/word_compare/origin_to_translation_page.dart';
 import 'package:open_words/view/game/word_compare/translation_to_origin_page.dart';
+import 'package:open_words/view/game/match_pairs/word_pairs/word_pairs_page.dart';
 import 'package:open_words/view/shared/list/adaptive_grid_view.dart';
 import 'package:open_words/view/shared/tile/text_tile.dart';
 
@@ -16,24 +18,60 @@ class GameListPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Games')),
       body: SafeArea(
-        child: AdaptiveGridView(
-          maxCrossAxisExtent: 400,
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
           children: [
-            _GameInfoTile(
-              name: 'Compare Origins',
-              needWords: 8,
-              count: group.words.length,
-              route: (builder) => OriginToTranslationPage(group: group),
-            ),
-            _GameInfoTile(
-              name: 'Compare Translations',
-              needWords: 8,
-              count: group.words.length,
-              route: (builder) => TranslationToOriginPage(group: group),
-            ),
+            _section(context, 'Compare', [
+              _GameInfoTile(
+                name: 'Origins',
+                needWords: 8,
+                count: group.words.length,
+                route: (builder) => OriginToTranslationPage(group: group),
+              ),
+              _GameInfoTile(
+                name: 'Translations',
+                needWords: 8,
+                count: group.words.length,
+                route: (builder) => TranslationToOriginPage(group: group),
+              ),
+            ]),
+            const SizedBox(height: 10),
+            _section(context, 'Match', [
+              _GameInfoTile(
+                name: 'Word Pairs',
+                needWords: 5,
+                count: group.words.length,
+                route: (builder) => WordPairsPage(group: group),
+              ),
+              _GameInfoTile(
+                name: 'Audio Pairs',
+                needWords: 5,
+                count: group.words.length,
+                route: (builder) => AudioPairsPage(group: group),
+              ),
+            ]),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _section(BuildContext context, String title, List<Widget> widgets) {
+    final theme = Theme.of(context);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title, style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold)),
+        const SizedBox(height: 10),
+        AdaptiveGridView(
+          maxCrossAxisExtent: 300,
+          mainAxisExtent: 120,
+          shrinkWrap: true,
+          children: widgets,
+        ),
+      ],
     );
   }
 }
@@ -66,9 +104,9 @@ class _GameInfoTile extends StatelessWidget {
 
     bool notEnough = _isNotEnough();
 
-    return Card(
+    return Card.outlined(
       child: TextTile(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(14.0),
         title: name,
         subtitle: notEnough ? _notEnoughMessage() : null,
         subtitleStyle: TextStyle(color: scheem.error),
