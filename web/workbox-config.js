af8ed8cfc19ab28f52facbb89f0c31f0d1312cde
sipasi <tanksasha77@gmail.com>
2024-07-31 01:44:48 +0300

    refactor: use build in flutter service-worker

--- web/workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  globDirectory: "build/web/",
-  globPatterns: ["**/*.{json,otf,ttf,js,wasm,png,ico,html}"],
-  swSrc: "web/service-worker.js",
-  swDest: "build/web/service-worker.js",
-  maximumFileSizeToCacheInBytes: 10000000,
-};
