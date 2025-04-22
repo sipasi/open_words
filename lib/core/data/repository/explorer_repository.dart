0c71b3f5a83677257f3710504a53b225ba71f1e4
sipasi <tanksasha77@gmail.com>
2025-04-23 00:01:44 +0300

    feat: add ExplorerRepository and implementation for folder-based data retrieval

---
+++ lib/core/data/repository/explorer_repository.dart
@@ -0,0 +1,61 @@
+import 'package:drift/drift.dart';
+import 'package:open_words/core/data/entities/explorer/explorer_data.dart';
+import 'package:open_words/core/data/entities/id.dart';
+import 'package:open_words/core/data/repository/folder_repository.dart';
+import 'package:open_words/core/data/repository/mappers/explorer_sql_mapper.dart';
+import 'package:open_words/core/data/repository/word_group_repository.dart';
+import 'package:open_words/core/data/sources/drift/app_drift_database.dart';
+
+sealed class ExplorerRepository {
+  Future<ExplorerData> allByFolder(Id folderId);
+}
+
+class ExplorerRepositoryImpl extends ExplorerRepository {
+  final WordGroupRepository groupRepository;
+  final FolderRepository folderRepository;
+
+  final AppDriftDatabase database;
+
+  ExplorerRepositoryImpl(
+    this.database,
+    this.groupRepository,
+    this.folderRepository,
+  );
+
+  @override
+  Future<ExplorerData> allByFolder(Id folderId) async {
+    final rows = await database.allByFolderQuery(folderId).get();
+
+    return ExplorerSqlMapper.fromList(rows);
+  }
+}
+
+extension _Queries on AppDriftDatabase {
+  Selectable<QueryRow> allByFolderQuery(Id folderId) {
+    return customSelect(_query(folderId));
+  }
+
+  static String _query(Id parentFolder) {
+    final predicate =
+        parentFolder.isEmpty ? 'IS NULL' : '= ${parentFolder.valueOrNull()}';
+
+    final template =
+        'SELECT id, parent_id AS folder_id, name, created, NULL AS modified, '
+        'NULL AS language_origin_code, NULL AS language_origin_name, NULL AS language_origin_native, '
+        'NULL AS language_translation_code, NULL AS language_translation_name, NULL AS language_translation_native, '
+        '\'Folder\' AS entity_type '
+        'FROM folders '
+        'WHERE parent_id $predicate '
+        ''
+        'UNION ALL '
+        ''
+        'SELECT id, folder_id, name, created, modified, '
+        'language_origin_code, language_origin_name, language_origin_native, '
+        'language_translation_code, language_translation_name, language_translation_native, '
+        '\'WordGroup\' AS entity_type '
+        'FROM word_groups '
+        'WHERE folder_id $predicate;';
+
+    return template;
+  }
+}
