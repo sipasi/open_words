3f78b72dbef66dcd579d0e18ab1c61cc826b08c5
sipasi <tanksasha77@gmail.com>
2025-04-23 00:02:13 +0300

    feat: update StorageSet and StorageFactory to include WordMetadataRepository and ExplorerRepository

--- lib/core/data/provider/storage_factory.dart
+++ lib/core/data/provider/storage_factory.dart
@@ -1,5 +1,7 @@
+import 'package:open_words/core/data/repository/explorer_repository.dart';
 import 'package:open_words/core/data/repository/folder_repository.dart';
 import 'package:open_words/core/data/repository/word_group_repository.dart';
+import 'package:open_words/core/data/repository/word_metadata_repository.dart';
 import 'package:open_words/core/data/repository/word_repository.dart';
 import 'package:open_words/core/data/sources/app_database.dart';
 import 'package:open_words/core/data/sources/drift/app_drift_database.dart';
@@ -9,12 +11,16 @@ class StorageSet {
   final FolderRepository folderRepository;
   final WordGroupRepository groupRepository;
   final WordRepository wordRepository;
+  final WordMetadataRepository wordMetadataRepository;
+  final ExplorerRepository explorerRepository;
 
   StorageSet({
     required this.database,
     required this.folderRepository,
     required this.groupRepository,
     required this.wordRepository,
+    required this.wordMetadataRepository,
+    required this.explorerRepository,
   });
 }
 
@@ -22,11 +28,16 @@ class StorageFactory {
   static Future<StorageSet> _drift() async {
     final database = AppDriftDatabase();
 
+    final groups = WordGroupRepositoryImpl(database);
+    final folders = FolderRepositoryImpl(database);
+
     return StorageSet(
       database: AppDatabaseImpl(database),
-      folderRepository: FolderRepositoryImpl(database),
-      groupRepository: WordGroupRepositoryImpl(database),
+      folderRepository: folders,
+      groupRepository: groups,
       wordRepository: WordRepositoryImpl(database),
+      wordMetadataRepository: WordMetadataRepositoryImpl(database),
+      explorerRepository: ExplorerRepositoryImpl(database, groups, folders),
     );
   }
 
