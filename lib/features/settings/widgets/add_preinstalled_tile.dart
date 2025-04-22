9739539765698822c9ca58e2fec8b6be81e2e3a1
sipasi <tanksasha77@gmail.com>
2025-04-23 02:48:54 +0300

    feat: enhance AddPreinstalledTile to create folder and add words from JSON data

--- lib/features/settings/widgets/add_preinstalled_tile.dart
+++ lib/features/settings/widgets/add_preinstalled_tile.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:open_words/core/data/draft/word_draft.dart';
 import 'package:open_words/core/data/entities/id.dart';
 import 'package:open_words/core/data/entities/language_info.dart';
+import 'package:open_words/core/data/repository/folder_repository.dart';
 import 'package:open_words/core/data/repository/word_group_repository.dart';
 import 'package:open_words/core/data/repository/word_repository.dart';
 import 'package:open_words/features/explorer/bloc/explorer_bloc.dart';
@@ -32,6 +34,7 @@ class AddPreinstalledTile extends StatelessWidget {
   }
 
   Future _addPreinstalled(BuildContext context) async {
+    final folderRepository = GetIt.I.get<FolderRepository>();
     final groupRepository = GetIt.I.get<WordGroupRepository>();
     final wordRepository = GetIt.I.get<WordRepository>();
 
@@ -41,27 +44,39 @@ class AddPreinstalledTile extends StatelessWidget {
 
     final jsonResult = jsonDecode(jsonData);
 
+    final folderName = 'Open Words Collection';
+
+    final folder =
+        await folderRepository.byName(folderName) ??
+        await folderRepository.create(
+          parentId: const Id.empty(),
+          name: folderName,
+        );
+
     for (var node in jsonResult) {
       final group = await groupRepository.create(
-        parentFolder: const Id.empty(),
+        parentId: folder.id,
         name: node['name'],
         origin: LanguageInfo.fromMap(node['origin']),
         translation: LanguageInfo.fromMap(node['translation']),
       );
 
-      for (var wordNode in node['words']) {
-        await wordRepository.create(
-          group: group.id,
-          origin: wordNode['origin'],
-          translation: wordNode['translation'],
-        );
-      }
+      await wordRepository.createAll(
+        groupId: group.id,
+        drafts:
+            List.of(node['words']).map((e) {
+              return WordDraft(
+                origin: e['origin'],
+                translation: e['translation'],
+              );
+            }).toList(),
+      );
     }
 
     if (!context.mounted) {
       return;
     }
 
-    context.read<ExplorerBloc>().add(ExplorerLoadRequested());
+    context.read<ExplorerBloc>().add(ExplorerRefreshRequested());
   }
 }
