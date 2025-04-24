c91bb59bb8149779f33ccfbc4fd3e9cf56a1d048
sipasi <tanksasha77@gmail.com>
2025-04-24 23:51:27 +0300

    refactor: update WordMetadataServiceDependency to register WordMetadataWebApi and improve API retrieval

--- lib/core/dependency/word_metadata_service_dependency.dart
+++ lib/core/dependency/word_metadata_service_dependency.dart
@@ -2,12 +2,15 @@ import 'package:get_it/get_it.dart';
 import 'package:open_words/core/dependency/app_dependency.dart';
 import 'package:open_words/core/services/word_metadata/dev_dictionary_api/dev_dictionary_api.dart';
 import 'package:open_words/core/services/word_metadata/word_metadata_service.dart';
+import 'package:open_words/core/services/word_metadata/word_metadata_web_api.dart';
 
 final class WordMetadataServiceDependency extends AppDependency {
   @override
   Future inject(GetIt container) {
+    container.registerSingleton<WordMetadataWebApi>(DevDictionaryApi());
+
     container.registerSingleton<WordMetadataService>(
-      WordMetadataService(repository: container.get(), api: DevDictionaryApi()),
+      WordMetadataService(repository: container.get(), api: container.get()),
     );
 
     return Future.value();
