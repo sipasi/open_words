e2f20f53ebf4fa05716ec0bdb09af7b2e454d388
sipasi <tanksasha77@gmail.com>
2025-04-26 01:12:49 +0300

    feat: implement methods for retrieving folder paths in FolderRepository

--- lib/core/data/repository/folder_repository.dart
+++ lib/core/data/repository/folder_repository.dart
@@ -1,5 +1,6 @@
 import 'package:drift/drift.dart';
 import 'package:open_words/core/data/entities/folder/folder.dart';
+import 'package:open_words/core/data/entities/folder/folder_path.dart';
 import 'package:open_words/core/data/entities/id.dart';
 import 'package:open_words/core/data/repository/mappers/folder_sql_mapper.dart';
 import 'package:open_words/core/data/sources/drift/app_drift_database.dart';
@@ -8,15 +9,17 @@ sealed class FolderRepository {
   Future<List<Folder>> all();
   Future<List<Folder>> allByParent(Id parent);
 
+  Future<List<FolderPath>> allPath();
+  Future<List<FolderPath>> allMovedPathFor(Id id);
+  Future<FolderPath?> onePath(Id id);
+
   Future<Folder?> byId(Id id);
   Future<Folder?> byName(String name);
 
+  Future delete(Id id);
+
   Future<Folder> create({required Id parentId, required String name});
-  Future<Folder> update({
-    required Id id,
-    required Id parentId,
-    required String name,
-  });
+  Future<Folder> update({required Id id, Id parentId, String name});
 }
 
 class FolderRepositoryImpl extends FolderRepository {
@@ -32,6 +35,27 @@ class FolderRepositoryImpl extends FolderRepository {
   @override
   Future<List<Folder>> allByParent(Id parentId) {
     return database.allByParentQuery(parentId).map(FolderSqlMapper.from).get();
+  }
+
+  @override
+  Future<List<FolderPath>> allPath() {
+    return database.allPathQuery().map(FolderSqlMapper.folderPath).get();
+  }
+
+  @override
+  Future<List<FolderPath>> allMovedPathFor(Id id) {
+    return database
+        .allMovedPathByQuery(id)
+        .map(FolderSqlMapper.folderPath)
+        .get();
+  }
+
+  @override
+  Future<FolderPath?> onePath(Id id) {
+    return database
+        .pathQuery(id)
+        .map(FolderSqlMapper.folderPath)
+        .getSingleOrNull();
   }
 
   @override
@@ -64,6 +88,8 @@ class FolderRepositoryImpl extends FolderRepository {
 
   @override
   Future<Folder> update({required Id id, Id? parentId, String? name}) async {
+    if (parentId != null) {}
+
     await database.managers.folders
         .filter((f) => f.id.equals(id.valueOrNull()))
         .update(
@@ -73,6 +99,13 @@ class FolderRepositoryImpl extends FolderRepository {
     final entity = await byId(id);
 
     return entity!;
+  }
+
+  @override
+  Future delete(Id id) {
+    return database.managers.folders
+        .filter((f) => f.id.equals(id.valueOrNull()))
+        .delete();
   }
 }
 
@@ -90,6 +123,18 @@ extension _Queries on AppDriftDatabase {
       _query(where: 'f.parent_id = ?'),
       variables: [Variable.withInt(parentId.valueOrThrow())],
     );
+  }
+
+  Selectable<QueryRow> allPathQuery() {
+    return customSelect(_allPathQuery());
+  }
+
+  Selectable<QueryRow> allMovedPathByQuery(Id id) {
+    return customSelect(_allMovedPathByQuery(id));
+  }
+
+  Selectable<QueryRow> pathQuery(Id id) {
+    return customSelect(_pathQuery(id));
   }
 
   Selectable<QueryRow> byIdQuery(Id id) {
@@ -114,5 +159,89 @@ extension _Queries on AppDriftDatabase {
     return where == null
         ? '$template ORDER BY f.name;'
         : '$template WHERE $where ORDER BY f.name;';
+  }
+
+  static String _allPathQuery() {
+    return '''
+WITH RECURSIVE FolderPath(id, parent_id, name, full_path) AS (
+    -- Base case: root folders
+    SELECT
+        id,
+        parent_id,
+        name,
+        name AS full_path
+    FROM folders
+    WHERE parent_id IS NULL
+
+    UNION ALL
+
+    -- Recursive step: append name to parent's full path
+    SELECT
+        f.id,
+        f.parent_id,
+        f.name,
+        fp.full_path || '/' || f.name AS full_path
+    FROM folders f
+    JOIN FolderPath fp ON f.parent_id = fp.id
+)
+SELECT * FROM FolderPath;
+''';
+  }
+
+  static String _allMovedPathByQuery(Id id) {
+    int? idValue = id.valueOrNull();
+
+    return '''
+WITH RECURSIVE FolderPath(id, parent_id, name, full_path) AS (
+    -- Base case: root folders
+    SELECT
+        id,
+        parent_id,
+        name,
+        name AS full_path
+    FROM folders
+    WHERE parent_id IS NULL
+
+    UNION ALL
+
+    -- Recursive step: append name to parent's full path
+    SELECT
+        f.id,
+        f.parent_id,
+        f.name,
+        fp.full_path || '/' || f.name AS full_path
+    FROM folders f
+    JOIN FolderPath fp ON f.parent_id = fp.id
+    WHERE f.id != $idValue
+)
+SELECT * FROM FolderPath;
+''';
+  }
+
+  static String _pathQuery(Id id) {
+    return '''
+WITH RECURSIVE FolderPath(id, parent_id, name, full_path) AS (
+    -- Start from the target folder
+    SELECT
+        id,
+        parent_id,
+        name,
+        name AS full_path
+    FROM folders
+    WHERE id = ${id.valueOrNull() ?? -1}
+
+    UNION ALL
+
+    -- Go upward toward the root
+    SELECT
+        f.id,
+        f.parent_id,
+        f.name,
+        f.name || '/' || fp.full_path
+    FROM folders f
+    JOIN FolderPath fp ON fp.parent_id = f.id
+)
+SELECT * FROM FolderPath;
+''';
   }
 }
