6e0e404f40f4aba908c26642dbfeccabbe8e7fde
sipasi <tanksasha77@gmail.com>
2025-04-26 01:12:18 +0300

    feat: add FolderPath entity to represent folder structure in the application

---
+++ lib/core/data/entities/folder/folder_path.dart
@@ -0,0 +1,15 @@
+import 'package:open_words/core/data/entities/id.dart';
+
+class FolderPath {
+  final Id folderId;
+  final Id parentId;
+  final String path;
+  final String name;
+
+  FolderPath({
+    required this.folderId,
+    required this.parentId,
+    required this.path,
+    required this.name,
+  });
+}
