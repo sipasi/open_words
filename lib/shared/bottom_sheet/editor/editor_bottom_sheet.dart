9845c2f3eec69bb03e8a4a062dd853cfdeb0b4d8
sipasi <tanksasha77@gmail.com>
2025-04-29 10:59:17 +0300

    refactor: change bottomBar type in EditorBottomSheet to Widget for flexibility

--- lib/shared/bottom_sheet/editor/editor_bottom_sheet.dart
+++ lib/shared/bottom_sheet/editor/editor_bottom_sheet.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:open_words/shared/bottom_sheet/editor/editor_bottom_bar.dart';
 import 'package:open_words/shared/bottom_sheet/pop_scope_bottom_sheet.dart';
 
 class EditorBottomSheet extends StatelessWidget {
   final Widget body;
-  final EditorBottomBar bottomBar;
+  final Widget bottomBar;
   final bool Function() showDismissDialog;
 
   const EditorBottomSheet({
