0071acfa6dff60bbc374228f315585c78da9e856
sipasi <tanksasha77@gmail.com>
2025-05-02 12:58:05 +0300

    refactor: simplify Result class by removing unused methods and improving type handling

--- lib/core/result/async_result.dart
+++ lib/core/result/async_result.dart
@@ -11,21 +11,4 @@ extension AsyncResultExtension<T> on AsyncResult<T> {
       return Result.failure(e.toString(), e);
     }
   }
-
-  static AsyncResult<T> value<T>(T value) async => Result.success(value);
-  static AsyncResult<T> error<T>(String message, [Object? error]) async =>
-      Result.failure(message, error);
-
-  Future<R> whenAsync<R>({
-    required R Function() onEmpty,
-    required R Function(T value) onSuccess,
-    required R Function(String message, Object? error) onFailure,
-  }) async {
-    final result = await this;
-    return result.when(
-      onEmpty: onEmpty,
-      onSuccess: onSuccess,
-      onFailure: onFailure,
-    );
-  }
 }
--- lib/core/result/result.dart
+++ lib/core/result/result.dart
@@ -5,34 +5,27 @@ part 'crud_result.dart';
 sealed class Result<T> {
   const Result();
 
-  factory Result.empty() => EmptyResult<T>();
+  factory Result.empty() => EmptyResult();
   factory Result.success(T value) => SuccessResult(value);
   factory Result.failure(String message, [Object? error]) =>
       FailureResult(message, error);
-
-  R when<R>({
-    required R Function() onEmpty,
-    required R Function(T value) onSuccess,
-    required R Function(String message, Object? error) onFailure,
-  }) {
-    return switch (this) {
-      EmptyResult<T>() => onEmpty(),
-      SuccessResult<T>(:final value) => onSuccess(value),
-      FailureResult<T>(:final message, :final error) => onFailure(
-        message,
-        error,
-      ),
-    };
-  }
 
   T? tryValue() => switch (this) {
     SuccessResult<T>(:final value) => value,
     _ => null,
   };
 
-  bool get isEmpty => this is EmptyResult<T>;
+  bool get isEmpty => this is EmptyResult;
   bool get isSuccess => this is SuccessResult<T>;
   bool get isFailure => this is FailureResult<T>;
+
+  void onSuccess(void Function(T value) callback) {
+    if (this is SuccessResult<T>) callback((this as SuccessResult<T>).value);
+  }
+
+  void onEmpty(void Function() callback) {
+    if (this is EmptyResult) callback();
+  }
 }
 
 final class EmptyResult<T> extends Result<T> {
