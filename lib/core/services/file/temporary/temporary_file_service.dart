713fe4406e70b892a36eddd9bfbd791fb458b6eb
sipasi <tanksasha77@gmail.com>
2025-05-11 15:56:54 +0300

    feat: implement TemporaryFileService for managing temporary files

---
+++ lib/core/services/file/temporary/temporary_file_service.dart
@@ -0,0 +1,63 @@
+import 'package:open_words/core/services/file/local_save/local_file.dart';
+import 'package:open_words/core/services/file/local_save/local_file_service.dart';
+import 'package:open_words/core/services/path/app_directory_extension.dart';
+import 'package:uuid/uuid.dart';
+
+/// A service for creating and managing temporary files.
+///
+/// Temporary files are created for scoped usage and automatically cleaned up when no longer needed.
+sealed class TemporaryFileService {
+  const TemporaryFileService();
+
+  /// Creates a temporary file and runs [onCreated] with it in a scoped context.
+  ///
+  /// The temporary file has the provided [name] and [extension]. If [name] is null,
+  /// a unique name be generated.
+  ///
+  /// The [onCreated] callback receives the [LocalFile] instance and is responsible
+  /// for performing all necessary operations within the scope.
+  ///
+  /// Implementation delete the file automatically after the callback completes.
+  Future withTemporaryFile({
+    String? name,
+    required String extension,
+    required Future Function(LocalFile temporary) onCreated,
+  });
+}
+
+final class TemporaryFileServiceImpl extends TemporaryFileService {
+  static const _folderName = 'Temporary files';
+
+  final LocalFileService localFileService;
+
+  final Uuid uuid;
+
+  TemporaryFileServiceImpl({required this.localFileService})
+    : uuid = const Uuid();
+
+  @override
+  Future withTemporaryFile({
+    String? name,
+    required String extension,
+    required Future Function(LocalFile temporary) onCreated,
+  }) async {
+    final localFile = await localFileService.createIn(
+      place: (directory) {
+        return directory.temporary.withSubPath(
+          label: _folderName,
+          parts: [_folderName],
+        );
+      },
+      name: _name(name, extension),
+      extension: extension,
+    );
+
+    await onCreated(localFile);
+
+    await localFile.delete();
+  }
+
+  String _name(String? name, String extension) {
+    return '${name ?? uuid.v4()}.$extension';
+  }
+}
