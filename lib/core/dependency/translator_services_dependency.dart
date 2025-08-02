9306506110af82219800e04a640295e32bc4d221
sipasi <tanksasha77@gmail.com>
2025-08-02 17:38:19 +0300

    chore: update TranslatorServicesDependency to use TranslatorTemplateStorage

--- lib/core/dependency/translator_services_dependency.dart
+++ lib/core/dependency/translator_services_dependency.dart
@@ -1,18 +1,18 @@
 import 'package:get_it/get_it.dart';
 import 'package:open_words/core/dependency/app_dependency.dart';
-import 'package:open_words/core/services/language/translation/translator_option_storage.dart';
 import 'package:open_words/core/services/language/translation/translator_service.dart';
+import 'package:open_words/core/services/language/translation/translator_template_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final class TranslatorServicesDependency extends AppDependency {
   @override
   Future inject(GetIt container) async {
-    final optionStorage = TranslatorOptionStorageImpl(
+    final optionStorage = TranslatorTemplateStorageImpl(
       await SharedPreferences.getInstance(),
     );
     final translatorUrl = TranslatorServiceImpl(optionStorage);
 
-    container.registerSingleton<TranslatorOptionStorage>(optionStorage);
+    container.registerSingleton<TranslatorTemplateStorage>(optionStorage);
     container.registerSingleton<TranslatorService>(translatorUrl);
   }
 }
