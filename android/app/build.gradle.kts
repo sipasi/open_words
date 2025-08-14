866df34fcebb1dec72800455e7ff20872a2571f0
sipasi <tanksasha77@gmail.com>
2025-08-15 01:21:02 +0300

    chore: update minSdk to use flutter.minSdkVersion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.openray.open_words"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
