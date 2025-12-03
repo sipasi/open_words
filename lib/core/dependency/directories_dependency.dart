db10a3f515c65f751e66667ce3ab85bd5c10b403
sipasi <tanksasha.work@gmail.com>
2025-12-03 03:12:52 +0200

    fix(DirectoriesDependency): handle path_provider exception on web

--- lib/core/dependency/directories_dependency.dart
+++ lib/core/dependency/directories_dependency.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 import 'package:open_words/core/dependency/app_dependency.dart';
 import 'package:open_words/core/services/path/app_directory.dart';
@@ -9,29 +10,50 @@ import 'package:path_provider/path_provider.dart';
 final class DirectoriesDependency extends AppDependency {
   @override
   Future inject(GetIt container) async {
+    if (kIsWeb || kIsWasm) {
+      container.registerSingleton(AppDirectoryProvider.notSupported());
+
+      return;
+    }
+
     container.registerSingleton(
       AppDirectoryProvider(
-        temporary: await _create('Temporary', getTemporaryDirectory()),
-        support: await _create('App Support', getApplicationSupportDirectory()),
+        temporary: await _create(
+          'Temporary',
+          getTemporaryDirectory,
+        ),
+        support: await _create(
+          'App Support',
+          getApplicationSupportDirectory,
+        ),
         documents: await _create(
           'App Documents',
-          getApplicationDocumentsDirectory(),
+          getApplicationDocumentsDirectory,
         ),
-        cache: await _create('App Cache', getApplicationCacheDirectory()),
-        downloads: await _createOrNull('Downloads', getDownloadsDirectory()),
+        cache: await _create(
+          'App Cache',
+          getApplicationCacheDirectory,
+        ),
+        downloads: await _createOrNull(
+          'Downloads',
+          getDownloadsDirectory,
+        ),
       ),
     );
   }
 
-  Future<AppDirectory> _create(String label, Future<Directory> future) async {
-    return AppDirectory.fromDirectory(label, await future);
+  Future<AppDirectory> _create(
+    String label,
+    Future<Directory> Function() future,
+  ) async {
+    return AppDirectory.fromDirectory(label, await future());
   }
 
   Future<AppDirectory?> _createOrNull(
     String label,
-    Future<Directory?> future,
+    Future<Directory?> Function() future,
   ) async {
-    final directory = await future;
+    final directory = await future();
 
     if (directory == null) {
       return null;
